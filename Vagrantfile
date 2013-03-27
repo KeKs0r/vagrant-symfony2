5292da7f90079d76c9c24736ac59986c254efdd3
Marc Höffl <marc.hoeffl@gmail.com>
2013-03-27 07:24:56 -0400

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,22 @@
+Vagrant::Config.run do |config|
+    config.vm.box     = "squeeze64puppet3"
+    config.vm.box_url = "http://l4bs.slynett.com/vagrant/squeeze64-puppet3.box"
+    
+    config.vm.customize [ "modifyvm", :id, "--name", "Default PHP 5.3 VM 525ffe16a92f77e2c5ff02870ab57c78" ]
+    config.vm.customize [ "modifyvm", :id, "--memory", "1024", "--cpus", "2" ]
+
+    config.vm.network :hostonly, "11.11.11.11"
+    config.vm.host_name = "dev.local"
+
+    config.vm.share_folder "vagrant", "/vagrant", "."
+
+        config.vm.share_folder "project", "/var/www/vhosts/dev.local", "..", :nfs => true, :create => true, :remount => true
+    
+    config.vm.provision :shell, :inline => "echo \"Europe/Berlin\" | sudo tee /etc/timezone && dpkg-reconfigure --frontend noninteractive tzdata"
+
+    config.vm.provision :puppet do |puppet|
+        puppet.manifests_path = "manifests"
+        puppet.module_path    = "modules"
+        puppet.manifest_file  = "base.pp"
+    end
+end
---
+++ manifests/base.pp
@@ -0,0 +1,187 @@
+file { "/etc/apt/sources.list.d/squeeze-backports.list":
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    content => "deb http://backports.debian.org/debian-backports squeeze-backports main",
+}
+
+
+exec { "import-gpg":
+    command => "/usr/bin/wget -q http://www.dotdeb.org/dotdeb.gpg -O -| /usr/bin/apt-key add -"
+}
+
+exec { "/usr/bin/apt-get update":
+    require => [File["/etc/apt/sources.list.d/squeeze-backports.list"], Exec["import-gpg"]],
+}
+
+
+
+class { "system": }
+
+file { "/etc/motd":
+    ensure  => file,
+    mode    => "0644",
+    owner   => "root",
+    group   => "root",
+    content => template("system/motd.erb"),
+}
+
+            system::package { "build-essential": }
+                system::package { "curl": }
+                system::package { "git-core": }
+                system::package { "vim": }
+                system::package { "yui-compressor": }
+    
+system::config { "bashrc":
+    name   => ".bashrc",
+    source => "/vagrant/files/system/bashrc",
+}
+
+
+class { "apache": }
+
+class { "apache::mod::php":
+    require => Package["php5"]
+}
+
+
+apache::mod { "rewrite": }
+apache::mod { "headers": }
+
+apache::vhost { "dev.local":
+    priority    => "50",
+    vhost_name  => "*",
+    port        => "80",
+    docroot     => "/var/www/vhosts/dev.local/",
+    serveradmin => "admin@dev.local",
+    template    => "system/apache-default-vhost.erb",
+    override    => "All",
+}
+
+file { "phpmyadmin-vhost-creation":
+    path    => "/etc/apache2/sites-enabled/phpmyadmin.conf",
+    ensure  => "/vagrant/files/apache/sites-enabled/phpmyadmin.conf",
+    require => [Package["php5"], Package["apache2"]],
+    owner   => "root",
+    group   => "root",
+}
+
+
+class { "mysql":
+    root_password => "",
+    require       => Exec["apt-update"],
+}
+
+
+class { "php": }
+
+file { "php5-ini-apache2-config":
+    path    => "/etc/php5/apache2/php.ini",
+    ensure  => "/vagrant/files/php/php.ini",
+    require => Package["php5"],
+}
+
+file { "php5-ini-cli-config":
+    path    => "/etc/php5/cli/php.ini",
+    ensure  => "/vagrant/files/php/php-cli.ini",
+    require => Package["php5"],
+}
+
+php::module { "common": }
+php::module { "dev": }
+
+    php::module { "mysql": }
+    php::module { "intl": }
+    php::module { "cli": }
+    php::module { "imagick": }
+    php::module { "gd": }
+    php::module { "xsl": }
+    php::module { "mcrypt": }
+    php::module { "curl": }
+    php::module { "xdebug": }
+    php::module { "imap": }
+    php::module { "apc":
+      module_prefix => "php5-",
+    }
+    php::module { "sqlite": }
+
+class { "pear": }
+
+pear::package { "PEAR": }
+pear::package { "PHPUnit": }
+
+# pear::channel { "phpunit":
+#     url => "pear.phpunit.de",
+# }
+# 
+# pear::channel { "symfony2":
+#     url     => "pear.symfony.com",
+#     require => Exec["pear-channel-phpunit"],
+# }
+# 
+# pear::channel { "symfony1":
+#     url     => "pear.symfony-project.com",
+#     require => Exec["pear-channel-symfony2"],
+# }
+# 
+# pear::channel { "components":
+#     url     => "components.ez.no",
+#     require => Exec["pear-channel-symfony1"],
+# }
+
+system::package { "phpmyadmin":
+    require => Package["php5"]
+}
+
+
+class { "ruby":
+    gems_version  => "latest"
+}
+
+system::package { "libsqlite3-dev":
+    require => Package["ruby"],
+}
+
+exec { "capifony-install":
+    command => "gem install -q --no-verbose --no-ri --no-rdoc capifony",
+    path    => "/bin:/usr/bin",
+    require => [
+        Package["ruby"],
+        Package["libsqlite3-dev"],
+    ],
+}
+
+
+
+class { "composer":
+    command_name => "composer",
+    target_dir   => "/usr/local/bin",
+    auto_update  => true
+}
+
+
+system::package { "zsh": }
+
+exec { "oh-my-zsh-install":
+    command => "git clone https://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh",
+    path    => "/bin:/usr/bin",
+    require => Package["zsh"],
+}
+
+exec { "default-zsh-shell":
+    command => "chsh -s /usr/bin/zsh vagrant",
+    unless  => "grep -E \"^vagrant.+:/usr/bin/zsh$\" /etc/passwd",
+    require => Package["zsh"],
+    path    => "/bin:/usr/bin",
+}
+
+file { "zshrc-file-creation":
+    path    => "/home/vagrant/.zshrc",
+    ensure  => "/vagrant/files/.zshrc",
+    require => Exec["oh-my-zsh-install"],
+    owner   => "vagrant",
+    group   => "vagrant",
+    replace => false,
+}
+
+
